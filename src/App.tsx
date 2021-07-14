c5721f1f342e7cfb3791dc3d4cccc861bef00aef
The24thDS <davidsima@windowslive.com>
2021-07-14 20:12:55 +0300

    setup: Added Chakra UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,70 @@
 import { useState } from 'react';
 
+import { Box, Button, Flex, Image, Text, Link } from '@chakra-ui/react';
+import { motion } from 'framer-motion';
+
 import logo from './logo.svg';
 
-import './App.css';
-
-function App() {
+const App = (): JSX.Element => {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((c) => c + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+    <Box>
+      <Flex
+        as="header"
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        h="100vh"
+        fontSize="3xl"
+      >
+        <motion.div
+          animate={{ rotateZ: 360 }}
+          transition={{
+            repeat: Infinity,
+            duration: 20,
+            ease: 'linear',
+          }}
+        >
+          <Image src={logo} alt="" h="40vmin" />
+        </motion.div>
+        <Text>Hello Vite + React + Typescript + Chakra UI!</Text>
+        <Text>
+          <Button
+            colorScheme="blue"
+            fontSize="xl"
+            onClick={() => setCount((c) => c + 1)}
           >
+            count is: {count}
+          </Button>
+        </Text>
+        <Text>
+          Edit <code>App.tsx</code> and save to test HMR updates.
+        </Text>
+        <Text>
+          <Link href="https://reactjs.org" isExternal color="#61dafb">
             Learn React
-          </a>
+          </Link>
           {' | '}
-          <a
-            className="App-link"
+          <Link
             href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
+            isExternal
+            color="#61dafb"
           >
             Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+          </Link>
+          {' | '}
+          <Link
+            href="https://www.typescriptlang.org/"
+            isExternal
+            color="#61dafb"
+          >
+            Typescript
+          </Link>
+        </Text>
+      </Flex>
+    </Box>
   );
-}
+};
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
+import { ChakraProvider } from '@chakra-ui/react';
 
-import './index.css';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
