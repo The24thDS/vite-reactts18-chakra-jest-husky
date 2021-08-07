9b933341b49dca5196da657bdd764396ec01b60d
The24thDS <davidsima@windowslive.com>
2021-08-07 15:04:25 +0300

    feat: Added transient options for emotion styled use

---
+++ src/utils/general.ts
@@ -0,0 +1,7 @@
+import { CreateStyled } from '@emotion/styled';
+
+export const transientOptions: Parameters<CreateStyled>[1] = {
+  shouldForwardProp: (propName: string) => !propName.startsWith('$'),
+};
+
+export default transientOptions;
