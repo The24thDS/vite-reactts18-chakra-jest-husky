d98ba1e55a979866d665e70c0f545a3e7e084650
The24thDS <davidsima@windowslive.com>
2021-08-07 19:00:08 +0300

    fix: Fixed Jest not using the correct parser for ts files

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   roots: ['<rootDir>/src'],
   transform: {
     '^.+\\.tsx$': 'ts-jest',
+    '^.+\\.ts$': 'ts-jest',
   },
   testRegex: '(/__tests__/.*.(test|spec)).(jsx?|tsx?)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
