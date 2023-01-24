fb5dac7033f44aafb44914986e68fed102537920
David <hello@david-sima.dev>
2023-01-24 23:03:04 +0200

    feat: updated all dependencies
    BREAKING CHANGE: updated React to v18

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
-import reactJsx from 'vite-react-jsx';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh(), reactJsx()],
+  plugins: [react()],
 });
