058aa056af1cbcc827aaba564f7998ec633bd467
David Sima <hello@david-sima.dev>
2023-01-24 23:07:18 +0200

    feat: updated all dependencies (#6)
    BREAKING CHANGE: updated React to v18

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 
 import App from './App';
 import theme from './theme';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const root = createRoot(container!);
+root.render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </ChakraProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,7 @@
 import { extendTheme, ThemeConfig } from '@chakra-ui/react';
 
 const config: ThemeConfig = {
-  initialColorMode: 'dark',
+  initialColorMode: 'system',
   useSystemColorMode: false,
 };
 
